a40c45d4ae8d381ccc673e09945d949d05b4d934
juanfa <j.marulanda1@utp.edu.co>
2026-03-07 13:07:28 -0500

    feat: add Swagger documentation and integrate PostgreSQL adapter for Prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../generated/prisma/client';
 
 @Injectable()
@@ -6,6 +7,13 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
