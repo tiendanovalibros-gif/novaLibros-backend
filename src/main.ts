b5d2439c460e50b6009e5d4fb6bbd9cb32e2f417
juan-ganan <juan.ganan@utp.edu.co>
2026-04-24 10:46:16 -0500

    fix(cors): remove trailing slashes from allowed origins

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,8 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://localhost:3001',
       'https://nova-libros-frontend.vercel.app',
-      'https://www.novalibros.app/',
-      'https://novalibros.app/',
+      'https://www.novalibros.app',
+      'https://novalibros.app',
     ],
     credentials: true,
   });
